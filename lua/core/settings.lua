d5f6a41494aa14b38d2657e75df43ac2d9380da9
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-10 23:56:52 +0100

    typescript-tools

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -3,27 +3,27 @@ local opt = vim.opt
 local g = vim.g
 
 -- UI
-opt.number = true         -- Show line numbers
+opt.number = true -- Show line numbers
 opt.relativenumber = true -- Relative line numbers
-opt.cursorline = true     -- Highlight the current line
-opt.termguicolors = true  -- Enable 24-bit RGB colors
+opt.cursorline = true -- Highlight the current line
+opt.termguicolors = true -- Enable 24-bit RGB colors
 
 -- Tabs & Indentation
-opt.tabstop = 2           -- Number of spaces a tab counts for
-opt.shiftwidth = 2        -- Number of spaces for autoindent
-opt.expandtab = true      -- Convert tabs to spaces
-opt.smartindent = true    -- Enable smart indentation
+opt.tabstop = 2 -- Number of spaces a tab counts for
+opt.shiftwidth = 2 -- Number of spaces for autoindent
+opt.expandtab = true -- Convert tabs to spaces
+opt.smartindent = true -- Enable smart indentation
 
 -- Searching
-opt.ignorecase = true     -- Case insensitive searching
-opt.smartcase = true      -- Case-sensitive if uppercase present
-opt.hlsearch = false      -- Don't highlight search results
+opt.ignorecase = true -- Case insensitive searching
+opt.smartcase = true -- Case-sensitive if uppercase present
+opt.hlsearch = false -- Don't highlight search results
 
 -- Splits
-opt.splitright = true     -- Open vertical splits to the right
-opt.splitbelow = true     -- Open horizontal splits below
+opt.splitright = true -- Open vertical splits to the right
+opt.splitbelow = true -- Open horizontal splits below
 
 -- Performance
-opt.lazyredraw = true     -- Only redraw when needed
-opt.updatetime = 300      -- Faster completion
-opt.timeoutlen = 500      -- Faster mappings
+opt.lazyredraw = true -- Only redraw when needed
+opt.updatetime = 300 -- Faster completion
+opt.timeoutlen = 500 -- Faster mappings

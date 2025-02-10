50de826cb5ff5c4748ed6fa6e56ce83ab2d8b183
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-10 18:45:29 +0100

    completion

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -13,25 +13,60 @@ function M.setup()
   })
 
   -- Configure LSP servers
-  require("mason-lspconfig").setup({
-    ensure_installed = {
-      "ts_ls",       -- TypeScript/JavaScript
-      "jsonls",         -- JSON
-      "bashls",         -- Bash
-    },
-    automatic_installation = true,
-  })
+  -- In lua/config/lsp.lua
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    "ts_ls",  -- Correct official server name
+    "jsonls",
+    "bashls",
+  },
+  automatic_installation = true,
+})
 
   local lspconfig = require("lspconfig")
-  local keymaps = require("config.lsp_keymaps") -- Import keymaps
+  local keymaps = require("config.lsp_keymaps")
+  local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+  -- Common LSP setup
+  local on_attach = function(client, bufnr)
+    client.server_capabilities.documentFormattingProvider = false
+    keymaps.set_lsp_keymaps(bufnr)
+    
+    -- Format on save with conform.nvim instead
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({ async = false })
+      end,
+    })
+  end
 
   -- Configure TypeScript LSP
+  -- Keep using tsserver as the server name
   lspconfig.ts_ls.setup({
-    on_attach = function(client, bufnr)
-      client.server_capabilities.documentFormattingProvider = false -- Disable built-in formatting
-      keymaps.set_lsp_keymaps(bufnr) -- Apply LSP keymaps
-    end,
-    flags = { debounce_text_changes = 150 },
+    on_attach = on_attach,
+    capabilities = capabilities,
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = "all",
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+        },
+        format = {
+          indentSize = vim.o.shiftwidth,
+          convertTabsToSpaces = vim.o.expandtab,
+          tabSize = vim.o.tabstop,
+        }
+      },
+      javascript = {
+        format = {
+          indentSize = vim.o.shiftwidth,
+          convertTabsToSpaces = vim.o.expandtab,
+          tabSize = vim.o.tabstop,
+        }
+      }
+    }
   })
 end
 

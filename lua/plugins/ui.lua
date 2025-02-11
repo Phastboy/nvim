b25eed84a9ed488c136b93f4b5a1c52a661ed800
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-11 22:12:50 +0100

    fix trouble

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,19 +1,45 @@
 return {
-	{ -- Better diagnostics list
-		"folke/trouble.nvim",
-		opts = { use_diagnostic_signs = true },
-		keys = {
-			{ "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
-			{ "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Issues" },
-			{ "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Issues" },
-		},
-	},
-	{ -- Highlight TODO comments
+	-- Highlight TODO comments
+	{
 		"folke/todo-comments.nvim",
-		opts = {
-			keywords = {
-				FIX = { icon = " ", color = "error" },
-				TODO = { icon = " ", color = "info" },
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
+	-- Add to existing plugins/ui.lua
+	{
+		"folke/trouble.nvim",
+		opts = {},
+		cmd = "Trouble",
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
 			},
 		},
 	},

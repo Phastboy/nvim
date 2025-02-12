7ae029a977e7a864fc7704e43697007218d3760f
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-12 19:25:57 +0100

    auto-pairs, todo-comment

--- lua/plugins/pairs.lua
+++ lua/plugins/pairs.lua
@@ -1,10 +1,6 @@
 return {
-  {
-    "windwp/nvim-autopairs", -- The plugin for auto-closing pairs
-    config = function()
-      require("nvim-autopairs").setup({
-        check_ts = true,  -- Enable treesitter integration for better context awareness
-      })
-    end,
-  },
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true,
+    opts={},
 }

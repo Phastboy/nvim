5f749f8a1733e82e732dce5a24c45eb5e65405ea
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-12 19:33:39 +0100

    report

--- lua/core/misc.lua
+++ lua/core/misc.lua
@@ -3,6 +3,13 @@ local set = vim.keymap.set
 set("n", "<C-s>", "<Cmd>w<CR>", { desc = "Save file" })
 set("n", "<leader>q", "<Cmd>q<CR>", { desc = "Exit file" })
 set("n", "<leader>wq", "<Cmd>wq<CR>", { desc = "Save and Exit file" })
+
+-- Track performance metrics
+vim.api.nvim_create_user_command("PerfReport", function()
+	local stats = require("lazy").stats()
+	local report = string.format("Startup: %dms (Loaded %d/%d plugins)", stats.startuptime, stats.loaded, stats.count)
+	print(report)
+end, {})
 set("n", "<leader>lp", function()
 	require("lazy").profile()
 end, { desc = "Lazy Profile" })

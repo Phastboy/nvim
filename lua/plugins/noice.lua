d2ca90947470005183574a1bb634560260f1b4dd
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-13 04:11:21 +0100

    noice (#29)

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,13 +1,11 @@
 return {
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-		opts = {
-			require("config.noice").setup(),
-		},
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  config = function()
+    require("config.noice").setup()
+  end,
 }

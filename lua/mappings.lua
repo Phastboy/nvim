4fe6d14522f379dc745892c2d62170c713888235
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2025-02-16 05:37:22 +0100

    ctrl + s to save

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 -- add yours here
 
@@ -8,7 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>q", "<Cmd>q<CR>", { desc = "Exit file" })
 map("n", "<leader>wq", "<Cmd>wq<CR>", { desc = "Save and Exit file" })
-  
+
 map("n", "<leader>dm", "<Cmd>Neogen<CR>", { desc = "Generate module docs" })
 
 map("n", "<leader>lp", function()
@@ -19,4 +19,4 @@ map("n", "K", vim.lsp.buf.hover, { desc = "Show Documentation" })
 map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
 map("n", "gr", "<cmd>Telescope lsp_references<cr>", { desc = "References" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map({ "n", "i" }, "<C-s>", "<cmd> w <cr>")
